36acff0c78b487e9e3fe56957b4dea0566cf0a94
DanielKimster <70424200+DanielKimster@users.noreply.github.com>
2020-09-20 14:42:51 -1000

    Adding sumofnumbers.js and index.html

---
+++ sumofnumbers.js
@@ -0,0 +1,31 @@
+function sumFor(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function sumWhile(arr) {
+  let counter = 0;
+  let sum = 0;
+  while (counter < arr.length) {
+    sum += arr[counter];
+    counter++;
+  }
+  return sum;
+}
+
+function sumRecursion(arr) {
+  if (arr.length > 1) return arr[arr.length - 1] + sumRecursion(arr.slice(0, arr.length - 1));
+  return arr[0];
+}
+
+function sumTheSimpleWay(arr) {
+  return _.reduce(arr, (memo, num) => (memo + num), 0);
+}
+const testArray = [0, 1, 2, 3, 4, 5, 6];
+console.log(sumFor(testArray));
+console.log(sumWhile(testArray));
+console.log(sumRecursion(testArray));
+console.log(sumTheSimpleWay(testArray));
